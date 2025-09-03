96ea5f7a4cbf96215c83321d196b02c36af4fcb2
moyeta-tech <octaviomoya05@gmail.com>
2025-09-03 17:09:45 -0300

    Ejercicio 1 corregido y Ejercicio 2 terminado

--- Octavio Moya/Ejercicio 1/index.js
+++ Octavio Moya/Ejercicio 1/index.js
@@ -6,28 +6,23 @@ const arreglo = []
 
 const app = express()
 
-function calcularPerimetro(a, b) {
-    let perimetro = 2 * (a + b)
-    arreglo.push(perimetro)
-    return perimetro
-}
-
-function calcularSuperficie(a, b) {
-    let superficie = a * b
-    arreglo.push(superficie)
-    return superficie
-}
-
-app.get('/calculos', (req, res) => {
     const a = 10
     const b = 10
+    let perimetro = 2 * (a + b)
 
-    const perimetro = calcularPerimetro(a, b)
-    const superficie = calcularSuperficie(a, b)
+    let superficie = a * b
+
+    arreglo.push({ perimetro, superficie })
+
+app.get('/calculos', (req, res) => {
 
     const tipo = a === b ? 'Cuadrado' : 'Rectángulo'
 
-    res.json({ perimetro, superficie, figura: tipo })
+    const calculos = arreglo.map(v => {
+        return `perimetro: ${v.perimetro}, superficie: ${v.superficie}`
+    })
+
+    res.json({ arreglo: calculos, figura: tipo })
 })
 
 
